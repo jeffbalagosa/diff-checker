998a6bbffeda90df966c494a222a2be69cc70e51
Jeff Balagosa <jeff.balagosa@gmail.com>
2024-06-12 00:53:15 -0700

    Add comparison logic toscript. Change flex properties of container. Move button and result outside of app container.

--- dist/script.js
+++ dist/script.js
@@ -7,5 +7,28 @@ var _a;
     document.getElementById('results').innerHTML = result;
 });
 function compareTexts(original, changed) {
-    return 'Comparison result will be displayed here.';
+    const originalLines = original.split('\n');
+    const changedLines = changed.split('\n');
+    let addedLines = 0;
+    let removedLines = 0;
+    let diffResult = originalLines.map((line, index) => {
+        if (line !== changedLines[index]) {
+            removedLines++;
+            return `<span style="color: red; text-decoration: line-through;">${line}</span>`;
+        }
+        return `<span>${line}</span>`;
+    }).join('<br>');
+    const additionalLines = changedLines.slice(originalLines.length);
+    if (additionalLines.length > 0) {
+        addedLines += additionalLines.length;
+        additionalLines.forEach(line => {
+            diffResult += `<br><span style="color: green;">${line}</span>`;
+        });
+    }
+    return `
+        ${diffResult}
+        <br><br>
+        <strong>Lines Removed:</strong> ${removedLines}<br>
+        <strong>Lines Added:</strong> ${addedLines}
+    `;
 }
