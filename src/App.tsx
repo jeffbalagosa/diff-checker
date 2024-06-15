867f426095810061ffe58285d0cec55099660db4
Jeff Balagosa <jeff.balagosa@gmail.com>
2024-06-14 17:46:14 -0700

    feat: Add ResultsPanels component for displaying comparison results

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 import { Button } from './components/ui/button';
 import TextPanels from './components/Containers/TextPanels/TextPanels';
+import ResultsPanels from './components/Containers/ResultsPanels/ResultsPanels';
 
 function App() {
+  const [showResults, setShowResults] = useState(false);
+
+  const handleClick = () => {
+    if (showResults) {
+      setShowResults(false);
+    } else {
+      setShowResults(true);
+    }
+  };
+
   return (
     <>
       <h1 className="text-orange-400 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl p-5">
@@ -11,10 +23,11 @@ function App() {
       </h1>
       <TextPanels />
       <div className="flex justify-center mt-6">
-        <Button className="bg-orange-400 text-black text-lg hover:bg-orange-500">
+        <Button className="bg-orange-400 text-black text-lg hover:bg-orange-500" onClick={handleClick}>
           Compare
         </Button>
       </div>
+      {showResults && <ResultsPanels />}
     </>
   );
 }
---
+++ src/components/Containers/ResultsPanels/ResultsPanels.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ResultsPanels = () => {
+  return (
+    <div className="flex justify-center mt-6 text-orange-400">
+        <div>
+            Test 1
+        </div>
+        <div>
+            Test 2
+        </div>
+    </div>
+  );
+};
+
+export default ResultsPanels;
