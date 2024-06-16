a5a04427fe5b6e036c69f58c0020a98b33eacca7
Jeff Balagosa <jeff.balagosa@gmail.com>
2024-06-16 15:04:29 -0700

    Remove placeholder:text-muted-foreground from ResultsPanel.

--- src/components/Containers/ResultsPanels/ResultsPanels.tsx
+++ src/components/Containers/ResultsPanels/ResultsPanels.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { cn } from '@/lib/utils';
+import React from "react";
+import { cn } from "@/lib/utils";
 
 interface ResultsPanelsProps {
   originalTextInput: string;
@@ -14,14 +14,14 @@ const ResultsPanels = ({
     <div className="grid grid-cols-2 gap-6">
       <div
         className={cn(
-          'flex-1 min-h-[80px] w-full pl-14 pr-3 py-2 rounded-md border border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-lg'
+          "flex-1 min-h-[80px] w-full pl-14 pr-3 py-2 rounded-md border border-input bg-background ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-lg"
         )}
       >
         {originalTextInput}
       </div>
       <div
         className={cn(
-          'flex-1 min-h-[80px] w-full pl-14 pr-3 py-2 rounded-md border border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-lg'
+          "flex-1 min-h-[80px] w-full pl-14 pr-3 py-2 rounded-md border border-input bg-background ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-lg"
         )}
       >
         {changedTextInput}
