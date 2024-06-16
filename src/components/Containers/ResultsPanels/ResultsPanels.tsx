a2c2a95cb1cb1830dbe6f5b3222c5eadb08aa312
Jeff Balagosa <jeff.balagosa@gmail.com>
2024-06-16 02:42:43 -0700

    refactor: Update ResultsPanels component to display original and changed text inputs side by side

--- src/components/Containers/ResultsPanels/ResultsPanels.tsx
+++ src/components/Containers/ResultsPanels/ResultsPanels.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { cn } from '@/lib/utils';
 
 interface ResultsPanelsProps {
   originalTextInput: string;
@@ -10,9 +11,21 @@ const ResultsPanels = ({
   changedTextInput,
 }: ResultsPanelsProps) => {
   return (
-    <div className="flex justify-center mt-6 text-orange-400">
-      <div>{originalTextInput}</div>
-      <div>{changedTextInput}</div>
+    <div className="grid grid-cols-2 gap-6">
+      <div
+        className={cn(
+          'flex-1 min-h-[80px] w-full pl-14 pr-3 py-2 rounded-md border border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-lg'
+        )}
+      >
+        {originalTextInput}
+      </div>
+      <div
+        className={cn(
+          'flex-1 min-h-[80px] w-full pl-14 pr-3 py-2 rounded-md border border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-lg'
+        )}
+      >
+        {changedTextInput}
+      </div>
     </div>
   );
 };
